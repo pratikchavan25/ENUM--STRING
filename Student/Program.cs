5f53b69a0e8754201dc27b93c039b061199a5fc8
Pratik <Pratik@DESKTOP-9189TTI>
2023-06-28 12:06:19 +0530

    Add project files.

---
+++ Student/Program.cs
@@ -0,0 +1,49 @@
+﻿
+//using Enum;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Text_in
+{ 
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            //Student1 s1 = new Student1("pratik", 123, 27, Stream.Mech);
+            //Console.WriteLine(s1.ToString());
+            string[] name = new string[5] { "Elon", "Jeff", "Rasika", "Janki", "Susim" };
+            string[] mobile = new string[6] { "Apple", "Samsung", "Redmi", "Oppo", "OnePlus" ,""};
+
+            foreach(string s in name)
+            {
+                Console.WriteLine(s);
+            }
+
+            Array.Sort(mobile);
+            Console.WriteLine("------after sort------");
+            foreach (var s in mobile) 
+            {
+                Console.WriteLine(s);
+            }
+
+            Array.Reverse(mobile);
+            Console.WriteLine("------after reverse------");
+            foreach(var s in mobile)
+            {
+                Console.WriteLine(s);
+            }
+
+            Array.Copy(name,2,mobile,2,1);
+            Console.WriteLine("------------------");
+            foreach (var item in mobile)
+            {
+                Console.WriteLine(item);
+            }
+
+        }
+    }
+}
+      
---
+++ Student/student.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ennum
+{
+    public enum Stream { CS, IT, Electrical, Electronic, Mech, Civil };
+
+    public class Student1
+    {
+        private string Name;
+        private int roll_no;
+        private int age;
+        // private char gender;
+        private Stream stream;
+        // private static int count;
+
+        public Student1(string Name, int roll_no, int age, Stream stream)
+        {
+
+            this.Name = Name;
+            this.roll_no = roll_no;
+            this.age = age;
+            this.stream = stream;
+        }
+        public override string ToString()
+        {
+            return $"Name is {Name}  Roll no is {roll_no} Age is {age} Stream is {stream}";
+        }
+
+    }
+
+}
+
+
+
+
+
